9fdd4e66f4d99fb222474aaae1fe84cfc1a1dcec
Charles K. Neimog <charlesneimog@outlook.com>
2025-10-23 08:43:41 -0300

    bump version

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 // Centralized configuration constants extracted from original render.js
 export const CONFIG = {
     VERSION_MAJOR: 0,
-    VERSION_MINOR: 2,
+    VERSION_MINOR: 3,
     VERSION_PATCH: 0,
-    VERSION_BUILD: 1,
+    VERSION_BUILD: 0,
 
     // Rendering
     ENABLE_WORD_HIGHLIGHT: true,

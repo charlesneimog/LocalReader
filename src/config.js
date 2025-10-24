5a3f50aca75801726e74237b31e2ef9001543be0
Charles K. Neimog <charlesneimog@outlook.com>
2025-10-24 12:51:59 -0300

    bump version

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ export const CONFIG = {
     VERSION_MAJOR: 0,
     VERSION_MINOR: 3,
     VERSION_PATCH: 0,
-    VERSION_BUILD: 5,
+    VERSION_BUILD: 6,
 
     // Rendering
     ENABLE_WORD_HIGHLIGHT: true,

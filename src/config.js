96a30f3a22e61f32f1b5bb64998a9f29201bc934
Charles K. Neimog <charlesneimog@outlook.com>
2025-11-05 12:11:48 -0300

    fix background color

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ export const CONFIG = {
     VERSION_MAJOR: 0,
     VERSION_MINOR: 7,
     VERSION_PATCH: 0,
-    VERSION_BUILD: 0,
+    VERSION_BUILD: 1,
 
     // Rendering
     ENABLE_WORD_HIGHLIGHT: true,

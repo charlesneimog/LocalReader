19ebe5143f4c6d73a6096d140629cd44fede7a6c
Charles K. Neimog <charlesneimog@outlook.com>
2026-04-27 08:36:30 -0300

    [version] bump version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "0.20.24+0";
+const APP_VERSION = "0.21.0+0";
 const IDB_VERSION = 1;
 const cacheName = `LocalReader-v${APP_VERSION}`;
 const runtimeCache = `LocalReader-runtime-v${APP_VERSION}`;

eb15c0588cc0bae9ee2354879cae0ebfccff0025
Charles K. Neimog <charlesneimog@outlook.com>
2026-07-27 12:32:07 -0300

    [version] bump

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ export const CONFIG = {
     VERSION_MAJOR: 0,
     VERSION_MINOR: 38,
     VERSION_PATCH: 7,
-    VERSION_BUILD: 19,
+    VERSION_BUILD: 20,
 
     // Rendering
     ENABLE_WORD_HIGHLIGHT: true,
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "0.38.7+19";
+const APP_VERSION = "0.38.7+20";
 const IDB_VERSION = 1;
 const cacheName = `PocketReader-v${APP_VERSION}`;
 const runtimeCache = `PocketReader-runtime-v${APP_VERSION}`;

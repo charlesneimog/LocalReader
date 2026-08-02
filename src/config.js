fad192be8587ebdb3e6f05c37661981eacb18de0
Charles K. Neimog <charlesneimog@outlook.com>
2026-08-02 20:58:46 -0300

    [version] bump

--- src/config.js
+++ src/config.js
@@ -27,7 +27,7 @@ export function normalizeInferenceBackend(value, fallback = INFERENCE_BACKENDS.W
 export const CONFIG = {
     VERSION_MAJOR: 0,
     VERSION_MINOR: 42,
-    VERSION_PATCH: 0,
+    VERSION_PATCH: 1,
     VERSION_BUILD: 0,
 
     // Rendering
